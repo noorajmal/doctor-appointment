3c7989e2d2b40d359710b83b46ee8738cfdcd59e
noorajmal <noormuhammadajmal@gmail.com>
2026-04-08 22:32:32 +0500

    Updated project (write your message)

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,44 +3,48 @@ import { useState } from 'react';
 
 const Login = () => {
 
-  const [state, setState] =useState('Sign Up')
+  const [state, setState] = useState('Sign Up')
   const [password, setPassword] = useState('');
-const [name ,setName] = useState('');
-const [email, setEmail] = useState('');
-const onSubmitHandler=async(e)=>{
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const onSubmitHandler = async (e) => {
 
-  e.preventDefault();
-}
+    e.preventDefault();
+  }
   return (
-    <form  className="min-h-[80vh] flex items-center justify-center" onSubmit={onSubmitHandler}>
+    <form className="min-h-[80vh] flex items-center justify-center" onSubmit={onSubmitHandler}>
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border border-gray-200 rounded-xl text-zinc-600 text-sm shadow-lg">
-        <p  className="text-2xl font-semibold">
-          {state==='Sign up'?"create account":"Login"}
+        <p className="text-2xl font-semibold">
+          {state === 'Sign up' ? "create account" : "Login"}
         </p>
         <p>
           Please {state === "Sign Up" ? "sign up" : "log in"} to book
           appointment
         </p>
-        <div className="w-full"><p>Full Name</p>
-        <input   className="border border-zinc-300 rounded w-full p-2 mt-1" type="text" onChange={(e) => setName(e.target.value)} value={name} required />
-        
-        </div>
-<div className="w-full"><p>Email</p>
-        <input   className="border border-zinc-300 rounded w-full p-2 mt-1" type="email" onChange={(e) => setEmail(e.target.value)} value={email} required />
-        
+
+        {
+          state === "Sign Up" && <div className="w-full"><p>Full Name</p>
+            <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="text" onChange={(e) => setName(e.target.value)} value={name} required />
+
+          </div>
+        }
+
+        <div className="w-full"><p>Email</p>
+          <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="email" onChange={(e) => setEmail(e.target.value)} value={email} required />
+
         </div>
 
         <div className="w-full"><p>Password</p>
-        <input   className="border border-zinc-300 rounded w-full p-2 mt-1" type="password" onChange={(e) => setPassword(e.target.value)} value={password} required />
-        
+          <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="password" onChange={(e) => setPassword(e.target.value)} value={password} required />
+
         </div>
-        <button  className="bg-primary text-white w-full py-2 rounded-md text-base" type="submit">
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base" type="submit">
           {state === "Sign Up" ? "create account" : "Log In"}
         </button>
 
 
 
-       {state === "Sign Up" ? (
+        {state === "Sign Up" ? (
           <p>
             Already have an account?{" "}
             <span
